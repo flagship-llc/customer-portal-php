021e16b9bfc4595e424caa7542bf06ae86c29071
跡部　達也 <atobe@flagship.cc>
2016-04-20 21:39:24 +0900

    TOPページの並び順を変更

--- portal/index.php
+++ portal/index.php
@@ -28,6 +28,118 @@ $subscription_id = $subscription->id;
                 </div>
                 <?php include("accountInfo.php") ?>
             </div> -->
+
+        <div data-cb-cancel-subscription-url="#">
+            <div class="cb-well">
+                <div id="cb-portal-subscription-title" class="page-header clearfix">
+                    <span class="h3">Subscription</span>
+                    <span class="cb-subscription-status <?php echo esc($subscription->status) ?>" >
+                        <?php echo ucfirst(str_replace("_", " ",  $subscription->status)) ?>
+                    </span>
+                    <?php
+                    if ($settingconfigData["changesubscription"]["allow"] == 'true'){
+                        $showEditDisplay = $servicePortal->getEditSubscription($settingconfigData);                    
+                        if($showEditDisplay==true) {   ?>
+                            <a href=<?php echo getEditUrl("editSubscription.php", $configData) ?>>
+                                <span class='glyphicon glyphicon-pencil'></span>Upgrade / Downgrade
+                            </a>
+                        <?php
+                            $showEditDisplay = false;
+                        }                    
+                    }               
+    ?>
+                </div>
+                <?php include("subscriptionInfo.php") ?>
+            </div>
+
+        <div class="cb-well">
+            <div id="cb-portal-subscription-info-title" class="page-header clearfix">
+                <span class="h4">Timeline</span>
+            </div>
+            <p class="text-muted">
+                <?php 
+                $phrase = $infoconfigData['Timeline']['Recurring_charge'];
+                $default = array('$planperiod', '$planunit');
+                $assign   = array($currentPlanDetails->period, $currentPlanDetails->periodUnit);
+                echo str_replace($default,  $assign, $phrase); ?> </p>
+                <?php if(isset($subscription->currentTermStart)){ ?>
+                    <p class="text-muted">
+                        <?php 
+                        $phrase = $infoconfigData['Timeline']['Current_term'];
+                        $default = array('$subscription.current_term_start', '$subscription.current_term_end');
+                        // $assign   = array(date('d-M-Y', $subscription->currentTermStart), date('d-M-Y', $subscription->currentTermEnd));
+                        // echo str_replace($default,  $assign, $phrase);
+
+                     ?> 
+                    </p> 
+                <?php } ?>
+                
+         <?php if ($subscription->status == "in_trial") {  ?>
+                <p class="text-muted">
+                    <?php echo str_replace('$subscription.trial_end', date('d-M-Y', $subscription->trialEnd),
+                                                $infoconfigData['Timeline']['Trial_ends_on']); ?> </p>
+                <?php } else if ($subscription->status == "cancelled") { ?>  
+                    <p class="text-muted">
+                        <?php echo str_replace('$subscription.cancelled_at', date('d-M-Y', $subscription->cancelledAt),
+                                                        $infoconfigData['Timeline']['Canceled_date']); ?> 
+                    </p> 
+                    <p class="text-muted">
+                        <?php echo str_replace('$subscription.trial_start', date('d-M-Y', $subscription->trialStart),
+                                                            $infoconfigData['Timeline']['Trial_started_on']); ?> 
+                    </p> 
+            <?php } else if ($subscription->status == "active") { ?>   
+                    <p class="text-muted">
+                        <?php echo str_replace('$subscription.current_term_end', date('d-M-Y', $subscription->currentTermEnd),
+                                                $infoconfigData['Timeline']['Next_billing_date']); ?> 
+                        <?php 
+                            if (isset($estimate->lineItems)) {
+                                foreach ($estimate->lineItems as $li) {
+                                    echo ' Your next plan will be '.$li->description.'.';
+                                }
+                            }
+                        ?>
+                    </p>
+                    <p class="text-muted">Your current billing term is 
+                        <?php echo date('d-M-Y', $subscription->currentTermStart) ?> and 
+                        <?php echo date('d-M-Y', $subscription->currentTermEnd) ?>. 
+                    </p>
+                    <p class="text-muted">
+                        <?php echo str_replace('$subscription.activated_at', date('d-M-Y', $subscription->activatedAt),
+                                    $infoconfigData['Timeline']['Activation_date']); ?> 
+                    </p>
+           <?php } else if ($subscription->status == "non_renewing") { ?>
+                    <p class="text-muted">
+                        <?php echo str_replace('$subscription.cancelled_at', date('d-M-Y', $subscription->currentTermEnd),
+                                                $infoconfigData['Non_Renewing_Subscriptions']['Will_be_canceled']); ?> 
+                    </p>
+                    <p class="text-muted"><?php echo str_replace('$subscription.activated_at', date('d-M-Y', $subscription->activatedAt),
+                                    $infoconfigData['Timeline']['Activation_date']); ?> </p>
+            <?php }  ?>
+            <p class="text-muted">
+                <?php echo str_replace('$subscription.created_at', date('d-M-Y', $subscription->createdAt),
+                                    $infoconfigData['Timeline']['Signed_up_on']); ?> 
+            </p>
+            <hr class="clearfix">
+            <div class="text-right">
+                <?php if($subscription->status == "cancelled") {?>
+                    <?php if($settingconfigData["reactivatesubscription"]["allow"] == 'true') {?>
+                        <a class="text-danger" data-cb-jshook="link-cancel-subscription" id="reactivateSubscription">Reactivate</a>
+                            this subscription
+                    <?php } ?>
+                <?php } else if ($settingconfigData["cancelsubscription"]["allow"] == 'true') { 
+                        if (!($subscription->status == "non_renewing" && 
+                                        $settingconfigData["cancelsubscription"]["immediately"] == "false")) { ?>
+                            <a class="text-danger" href=<?php echo getEditUrl("skip_a_month_view.php", $configData) ?> >Skip a Month</a> | 
+                            <a class="text-danger" href=<?php echo getEditUrl("cancelSubscription.php", $configData) ?> 
+                                id="cancelSubscription">
+                                Cancel
+                            </a> this subscription
+                      <?php } ?>
+                <?php } ?>
+
+
+            </div>
+        </div>
 			
             <div id="cb-portal-payment-mode" class="cb-well">
                 <div id="cb-portal-payment-mode-title" class="page-header clearfix">
@@ -107,118 +219,6 @@ $subscription_id = $subscription->id;
             <?php } else { ?>
                 <?php include("paymentHistoryInfo.php") ?>
             <?php } ?>
-        </div>
-    </div>
-
-    <div data-cb-cancel-subscription-url="#">
-        <div class="cb-well">
-            <div id="cb-portal-subscription-title" class="page-header clearfix">
-                <span class="h3">Subscription</span>
-                <span class="cb-subscription-status <?php echo esc($subscription->status) ?>" >
-                    <?php echo ucfirst(str_replace("_", " ",  $subscription->status)) ?>
-                </span>
-                <?php
-                if ($settingconfigData["changesubscription"]["allow"] == 'true'){
-                    $showEditDisplay = $servicePortal->getEditSubscription($settingconfigData);                    
-                    if($showEditDisplay==true) {   ?>
-                        <a href=<?php echo getEditUrl("editSubscription.php", $configData) ?>>
-                            <span class='glyphicon glyphicon-pencil'></span>Upgrade / Downgrade
-                        </a>
-                    <?php
-                        $showEditDisplay = false;
-                    }                    
-                }               
-?>
-            </div>
-            <?php include("subscriptionInfo.php") ?>
-        </div>
-
-    <div class="cb-well">
-        <div id="cb-portal-subscription-info-title" class="page-header clearfix">
-            <span class="h4">Timeline</span>
-        </div>
-        <p class="text-muted">
-            <?php 
-            $phrase = $infoconfigData['Timeline']['Recurring_charge'];
-            $default = array('$planperiod', '$planunit');
-            $assign   = array($currentPlanDetails->period, $currentPlanDetails->periodUnit);
-            echo str_replace($default,  $assign, $phrase); ?> </p>
-            <?php if(isset($subscription->currentTermStart)){ ?>
-                <p class="text-muted">
-                    <?php 
-                    $phrase = $infoconfigData['Timeline']['Current_term'];
-                    $default = array('$subscription.current_term_start', '$subscription.current_term_end');
-                    // $assign   = array(date('d-M-Y', $subscription->currentTermStart), date('d-M-Y', $subscription->currentTermEnd));
-                    // echo str_replace($default,  $assign, $phrase);
-
-                 ?> 
-                </p> 
-            <?php } ?>
-			
-     <?php if ($subscription->status == "in_trial") {  ?>
-            <p class="text-muted">
-                <?php echo str_replace('$subscription.trial_end', date('d-M-Y', $subscription->trialEnd),
-											$infoconfigData['Timeline']['Trial_ends_on']); ?> </p>
-            <?php } else if ($subscription->status == "cancelled") { ?>  
-                <p class="text-muted">
-					<?php echo str_replace('$subscription.cancelled_at', date('d-M-Y', $subscription->cancelledAt),
-													$infoconfigData['Timeline']['Canceled_date']); ?> 
-				</p> 
-                <p class="text-muted">
-            		<?php echo str_replace('$subscription.trial_start', date('d-M-Y', $subscription->trialStart),
-							 							$infoconfigData['Timeline']['Trial_started_on']); ?> 
-				</p> 
-        <?php } else if ($subscription->status == "active") { ?>   
-            	<p class="text-muted">
-                	<?php echo str_replace('$subscription.current_term_end', date('d-M-Y', $subscription->currentTermEnd),
-											$infoconfigData['Timeline']['Next_billing_date']); ?> 
-                    <?php 
-                        if (isset($estimate->lineItems)) {
-                            foreach ($estimate->lineItems as $li) {
-                                echo ' Your next plan will be '.$li->description.'.';
-                            }
-                        }
-                    ?>
-				</p>
-            	<p class="text-muted">Your current billing term is 
-					<?php echo date('d-M-Y', $subscription->currentTermStart) ?> and 
-                	<?php echo date('d-M-Y', $subscription->currentTermEnd) ?>. 
-				</p>
-            	<p class="text-muted">
-                	<?php echo str_replace('$subscription.activated_at', date('d-M-Y', $subscription->activatedAt),
-								$infoconfigData['Timeline']['Activation_date']); ?> 
-				</p>
-       <?php } else if ($subscription->status == "non_renewing") { ?>
-            	<p class="text-muted">
-                	<?php echo str_replace('$subscription.cancelled_at', date('d-M-Y', $subscription->currentTermEnd),
-											$infoconfigData['Non_Renewing_Subscriptions']['Will_be_canceled']); ?> 
-				</p>
-            	<p class="text-muted"><?php echo str_replace('$subscription.activated_at', date('d-M-Y', $subscription->activatedAt),
-								$infoconfigData['Timeline']['Activation_date']); ?> </p>
-        <?php }  ?>
-        <p class="text-muted">
-            <?php echo str_replace('$subscription.created_at', date('d-M-Y', $subscription->createdAt),
-								$infoconfigData['Timeline']['Signed_up_on']); ?> 
-		</p>
-        <hr class="clearfix">
-        <div class="text-right">
-			<?php if($subscription->status == "cancelled") {?>
-				<?php if($settingconfigData["reactivatesubscription"]["allow"] == 'true') {?>
-					<a class="text-danger" data-cb-jshook="link-cancel-subscription" id="reactivateSubscription">Reactivate</a>
-						this subscription
-			    <?php } ?>
-            <?php } else if ($settingconfigData["cancelsubscription"]["allow"] == 'true') { 
-                	if (!($subscription->status == "non_renewing" && 
-									$settingconfigData["cancelsubscription"]["immediately"] == "false")) { ?>
-                        <a class="text-danger" href=<?php echo getEditUrl("skip_a_month_view.php", $configData) ?> >Skip a Month</a> | 
-                        <a class="text-danger" href=<?php echo getEditUrl("cancelSubscription.php", $configData) ?> 
-							id="cancelSubscription">
-							Cancel
-						</a> this subscription
-                  <?php } ?>
-            <?php } ?>
-
-
         </div>
     </div>
 
