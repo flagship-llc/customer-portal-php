8f2b24133098ff8c9ee0c882d22d69910aed49f5
跡部　達也 <atobe@flagship.cc>
2016-04-26 20:42:36 +0900

    テスト変数

--- portal/paymentMode.php
+++ portal/paymentMode.php
@@ -7,18 +7,20 @@ if($customer->autoCollection == "on" && $customer->cardStatus == "no_card") {
 	$paymentMessage = $infoconfigData['Payment_Mode']['Offline_payment'];
 }
 ?>
-                
+            
+<?php var_dump($customer); ?>    
 <div class="form-horizontal">
 	<div class="text-center">
     	<div class="alert alert-info">
+            <?php var_dump($customer->cardStatus); ?>
         	<div class="media text-left">
-            	<span class="glyphicon glyphicon-info-sign pull-left"></span>
-                	<div class="media-body">
-                        <?php var_dump($customer->cardStatus); ?>
-                        <?php var_dump($customer->autoCollection); ?>
-                        <?php var_dump($paymentMessage); ?>
-                    	<?php echo $paymentMessage ?>
-                    </div>
+                <span class="glyphicon glyphicon-info-sign pull-left"></span>
+                <div class="media-body">
+                    <?php var_dump($customer->cardStatus); ?>
+                    <?php var_dump($customer->autoCollection); ?>
+                    <?php var_dump($paymentMessage); ?>
+                    <?php echo $paymentMessage ?>
+                </div>
             </div>
         </div>
     </div>
