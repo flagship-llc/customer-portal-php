e9f15d62445ef3ed48a7af27eb0d71e9dbe4f2de
跡部　達也 <atobe@flagship.cc>
2016-04-27 12:41:07 +0900

    skip a monthで伸びる期限を2ヶ月後の1日に修正

--- portal/skip_a_month.php
+++ portal/skip_a_month.php
@@ -12,18 +12,26 @@
         $subscription_term_end = $subscription->currentTermEnd;
         
         $end_day = date('d',$subscription_term_end);    
-        $end_month = date('m',$subscription_term_end);
+        $end_current_month = date('m',$subscription_term_end);
+        $end_month = date('m',$subscription_term_end) + 1;
         $end_year = date('Y',$subscription_term_end);
-        $last_day = date('t', mktime(0, 0, 0, $end_month, 1, $end_year));
-        $next_add_day ='+ '.($last_day - $end_day + 1).' day';
 
-        $one_month_after_end = strtotime( $next_add_day.'+ 7 hour', $subscription_term_end) ;
+        if($end_month == 13 ){
+            $end_month = 1;
+            $end_year = $end_year + 1;
+        }
 
+        $current_last_day = date('t', mktime(0, 0, 0, $end_current_month, 1, $end_year));
+        $next_last_day = date('t', mktime(0, 0, 0, $end_month, 1, $end_year));
+        $next_add_day =$current_last_day - $end_day + 1;
+        $next_term = $next_last_day + $next_add_day;
+        $one_month_after_end = strtotime( '+ '.$next_term .' day + 7 hour', $subscription_term_end) ;
+        
         $result = ChargeBee_Subscription::changeTermEnd($subscription_id, array(
         "termEndsAt" => $one_month_after_end));
         $subscription = $result->subscription();
         $customer = $result->customer();
-        $card = $result->card();
+        $card = $result->card();        
     }
 
 ?>
@@ -34,6 +42,7 @@
     <?php endif;?>
 
     <?php include("footer.php"); ?>
+    
     <script>
         function jump(){
             location.href = "index.php";
