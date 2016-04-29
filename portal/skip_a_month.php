d33afab19bd003467a66c9ff4702467c096134f5
跡部　達也 <atobe@flagship.cc>
2016-04-29 21:06:26 +0900

    skip a monthを２ヶ月後ではなく翌月に変更

--- portal/skip_a_month.php
+++ portal/skip_a_month.php
@@ -16,26 +16,14 @@
         $end_month = date('m',$subscription_term_end) + 1;
         $end_year = date('Y',$subscription_term_end);
 
-        if($end_month == 13 ){
-            $end_month = 1;
-            $end_year = $end_year + 1;
-        }
-
         $current_last_day = date('t', mktime(0, 0, 0, $end_current_month, 1, $end_year));
         $next_last_day = date('t', mktime(0, 0, 0, $end_month, 1, $end_year));
         $next_add_day =$current_last_day - $end_day + 1;
-        $next_term = $next_last_day + $next_add_day;
-        if($past_skip_true){
-            $one_month_after_end = strtotime( '+ '.$next_add_day .' day + 7 hour', $subscription_term_end) ;
-        }else{
-            $one_month_after_end = strtotime( '+ '.$next_term .' day + 7 hour', $subscription_term_end) ;
-        }
+        
+        $one_month_after_end = strtotime( '+ '.$next_add_day .' day + 7 hour', $subscription_term_end) ;
         
         $result = ChargeBee_Subscription::changeTermEnd($subscription_id, array(
         "termEndsAt" => $one_month_after_end));
-        $subscription = $result->subscription();
-        $customer = $result->customer();
-        $card = $result->card();        
     }
 
 ?>
