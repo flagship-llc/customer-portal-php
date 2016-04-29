ce903d6e9ada17155a2e9ffbd2f7f76094fdcf0f
跡部　達也 <atobe@flagship.cc>
2016-04-29 21:21:34 +0900

    payment historyページ送り調整

--- portal/invoiceTable.php
+++ portal/invoiceTable.php
@@ -75,4 +75,14 @@
         <span class="glyphicon glyphicon-exclamation-sign text-warning"></span>&nbsp;<small>Payment Due</small>&emsp;
         <span class="glyphicon glyphicon-remove text-danger"></span>&nbsp;<small>Not Paid</small>
     </p>
+
+    <p class="pull-right center-xs">
+        Showing <span class="inv-start-no"><?php echo $lastInvoiceNo + 1 ?></span> - 
+				<span class="inv-end-no"><?php echo $lastInvoiceNo + $count ?></span> of <span class="inv-max-no"><?php echo $maxInvoiceNo; ?></span>　
+        <?php if(isset($nextOffset)){ ?>
+            <a href="" data-cb-nav="next" id="next" class="text-orange">Next</a>
+        <?php } else{ ?>
+        <?php } ?>
+		<span class="inv-next-offset" style="display :none"><?php echo $customerInvoice->nextOffset() ?> </span>
+    </p>
 </div>
