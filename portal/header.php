56a1a4b65e3fbdab564986d27dfe8fc24176fb2e
跡部　達也 <atobe@flagship.cc>
2016-05-15 04:45:18 +0900

    GAコードを更新

--- portal/header.php
+++ portal/header.php
@@ -30,15 +30,15 @@ var b=document.getElementsByTagName("script")[0];
 a.src=document.location.protocol+"//script.crazyegg.com/pages/scripts/0042/6966.js?"+Math.floor(new Date().getTime()/3600000);
 a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
 </script>
-<script type="text/javascript">
+<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','__gaTracker');
-  __gaTracker('create', 'UA-60081243-3', 'auto');
-  __gaTracker('set', 'forceSSL', true);
-  __gaTracker('require', 'displayfeatures');
-  __gaTracker('send','pageview');
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+​
+  ga('create', 'UA-60081243-3', 'auto');
+  ga('send', 'pageview');
+​
 </script>
 
 </head>
