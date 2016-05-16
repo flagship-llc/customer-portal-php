f895be49471e933c5a6b219b3a486c8e64a45cad
跡部　達也 <atobe@flagship.cc>
2016-05-16 17:50:50 +0900

    crazyeggのコードを挿入

--- portal/header.php
+++ portal/header.php
@@ -23,6 +23,13 @@ include_once('brand-switch.php');
       <link rel="stylesheet" type="text/css" href="stylesheets/yume-theme.css">
     <?php }?>
 
+<script type="text/javascript">
+setTimeout(function(){var a=document.createElement("script");
+var b=document.getElementsByTagName("script")[0];
+a.src=document.location.protocol+"//script.crazyegg.com/pages/scripts/0042/6966.js?"+Math.floor(new Date().getTime()/3600000);
+a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+</script>
+
 
 <script type="text/javascript">
 setTimeout(function(){var a=document.createElement("script");
