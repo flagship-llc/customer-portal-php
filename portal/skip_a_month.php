de42abdd2fdcf0c74c2e5944c2c8fba07b26b351
跡部　達也 <atobe@flagship.cc>
2016-05-25 13:48:11 +0900

    JTCに直してからskip a monthの処理を実行

--- portal/skip_a_month.php
+++ portal/skip_a_month.php
@@ -6,10 +6,12 @@
     $subscription = $servicePortal->getSubscription();
     $customerInvoice = $servicePortal->retrieveInvoice();
 
+    date_default_timezone_set('UTC');
     include("skip_true.php");
     if($skip_st){
         $subscription_id = $subscription->id;
         $subscription_term_end = $subscription->currentTermEnd;
+        $subscription_term_end = strtotime( '+ 9 hour',$subscription_term_end);
         
         $end_day = date('d',$subscription_term_end);    
         $end_current_month = date('m',$subscription_term_end);
@@ -19,9 +21,7 @@
         $current_last_day = date('t', mktime(0, 0, 0, $end_current_month, 1, $end_year));
         $next_last_day = date('t', mktime(0, 0, 0, $end_month, 1, $end_year));
         $next_add_day =$current_last_day - $end_day + 1;
-        
-        $one_month_after_end = strtotime( '+ '.$next_add_day .' day + 9 hour', $subscription_term_end) ;
-        
+        $one_month_after_end = strtotime( '+ '.$next_add_day.' day - 9 hour', $subscription_term_end) ;
         $result = ChargeBee_Subscription::changeTermEnd($subscription_id, array(
         "termEndsAt" => $one_month_after_end));
     }
