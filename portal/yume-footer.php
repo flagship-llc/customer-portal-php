3b9928d087535be5c1a4210c778166ea9f3c8c07
跡部　達也 <atobe@flagship.cc>
2016-06-09 10:31:00 +0900

    Revert "YumeTwinsで追加したヘッダー・フッターのメニューを反映"
    This reverts commit de97ced999932c8fa534675cb1a68603d6961818.

--- portal/yume-footer.php
+++ portal/yume-footer.php
@@ -1,36 +1,3 @@
 
         
-        <div class="mailmagazine-area">
-            <div class="container" id="edit">
-                <ul class="footer-menu-top clearfix text-bold">
-                    <li><a href="https://yumetwins.com/" target="_blank">HOME</a></li>
-                    <li><a href="https://yumetwins.com/subscribe/" target="_blank">SUBSCRIBE</a></li>
-                    <li><a href="https://yumetwins.com/items/" target="_blank">ITEMS</a></li>
-                    <li><a href="https://yumetwins.com/about" target="_blank">ABOUT</a></li>
-                    <li><a href="http://help.tokyotreat.com/hc/en-us" target="_blank">HELP CENTER</a></li>
-                    <li><a href=<?php echo getLogoutUrl($configData); ?>>LOGOUT</a></li>
-                </ul>
-            </div>
-        </div>
-
-
-        <div class="container text-center orange footer-copyright-area">
-            <div class="footer-menu">
-                <ul class="text-bold">
-                    <li><a href="https://tokyotreat.com/privacy-policy/" target="_blank">Privacy Policy</a></li>
-                    <li><a href="https://tokyotreat.com/terms-of-service/" target="_blank">Terms of Service</a></li>
-                </ul>
-            </div>
-            <div class="social">
-                <ul>
-                  <li><a href="https://www.facebook.com/YumeTwins" target="_blank" class="fb"></a></li>
-                  <li><a href="https://twitter.com/YumeTwins" target="_blank" class="twitter"></a></li>
-                  <li><a href="https://www.instagram.com/YumeTwins/" target="_blank" class="instagram"></a></li>
-                  <li><a href="https://www.pinterest.com/tokyotreat/" target="_blank" class="pinterest"></a></li>
-                  <li><a href="https://www.youtube.com/results?search_query=tokyotreat" target="_blank" class="youtube"></a></li>
-                </ul>
-            </div>
-
             <div class="copyright col-xs-12">Copyright © 2016 TOKYOTREAT. All Rights Reserved.</div>
-
-        </div>
